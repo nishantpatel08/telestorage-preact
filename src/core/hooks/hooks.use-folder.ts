fb7cd1774104721ecba5267fffbe941a846aa7ce
nishant7929 <nishantpatel7929@gmail.com>
2025-04-22 01:52:13 +0530

    Update grid issue, map messages for media

--- src/core/hooks/hooks.use-folder.ts
+++ src/core/hooks/hooks.use-folder.ts
@@ -4,7 +4,7 @@ import { useStoreState } from 'unistore-hooks'
 import type { State, Folder, Message } from '~/core/store'
 import { useTexts } from '~/core/hooks'
 import { useUpdatableRef } from '~/tools/hooks'
-import { groupMessages } from '~/tools/handle-content'
+// import { groupMessages } from '~/tools/handle-content'
 
 export const useFolder = (id = '') => {
   const { texts } = useTexts('storage')
@@ -25,7 +25,8 @@ export const useFolder = (id = '') => {
 
   return useMemo(() => {
     const messages = [...(folderMessages || new Map()).values()] as Message[]
-    const groupedMessages = groupMessages(messages) as Message[]
+    // const groupedMessages = groupMessages(messages) as Message[]
+    const groupedMessages = messages.filter(message => message.media)
     return {
       folder: {
         ...folder,
